8d25d4120db71cc02899bc23687946b3bf4882c2
Jeff Ray <jeffmlazo@yahoo.com>
2021-07-30 00:44:09 +0800

    Add reactjs eslinters

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,8 +1,18 @@
 module.exports = {
-  plugins: ["mui-unused-classes"],
+  plugins: ["mui-unused-classes", "react"],
   env: {
     browser: true,
     node: true,
     es6: true,
+  },
+  extends: [
+    "airbnb-base",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
   },
 };
